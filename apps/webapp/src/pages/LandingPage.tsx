ab9125bb8920b64025c7db2c0fbc9e29ec6ec343
aisenhub <s17863657717@gmail.com>
2026-09-14 15:09:24 +0800

    refactor: separate public and product web boundaries

--- apps/webapp/src/pages/LandingPage.tsx
+++
@@ -1,9 +0,0 @@
-import LandingContent from "../features/marketing/components/LandingContent";
-
-interface Props {
-  onNavigate: (page: number) => void;
-}
-
-export default function LandingPage(props: Props) {
-  return <LandingContent {...props} />;
-}
--- apps/webapp/src/pages/TutorialsPage.tsx
+++
@@ -1,9 +0,0 @@
-import TutorialsContent from "../features/tutorials/components/TutorialsContent";
-
-interface TutorialsPageProps {
-  onNavigate: (page: number) => void;
-}
-
-export default function TutorialsPage({ onNavigate }: TutorialsPageProps) {
-  return <TutorialsContent onNavigate={onNavigate} />;
-}
